635be8d0d4ba7e02b840f2644b81991076aeffe6
Michal Daniluk <mdaniluk123@gmail.com>
2015-12-08 20:23:49 +0000

    Ex 6 fixed for more dimensions

--- ex6.m
+++ ex6.m
@@ -4,76 +4,88 @@ close all;
 toPlot = 0;
 nbValueGamma=10;
 vectorGamma=zeros(1,nbValueGamma);
-nLoops = 1;
+nLoops = 200;
 MSETrain=zeros(1,nbValueGamma);
 MSETest=zeros(1,nbValueGamma);
+MSEValidation=zeros(2,nbValueGamma);
 averageTestError = zeros(1,nbValueGamma);
 averageTrainError = zeros(1,nbValueGamma);
+averageValidationError=zeros(1,nbValueGamma);
+dimension = 10;
+wTrained=zeros(dimension,nbValueGamma);
+nTableTrainPoints=[10,100];
+resultsMatrix=zeros(2);
+indexaverage=zeros(1,2);
+for j=1:2
+    for seed = 1:nLoops
+        gamma=10^-6;
+        for i =1:nbValueGamma
+            s = RandStream('mt19937ar','Seed',seed);
+            RandStream.setGlobalStream(s);
 
-for seed = 1:nLoops
-    gamma=10^-6;
-    for i =1:nbValueGamma
-        s = RandStream('mt19937ar','Seed',seed);
-        RandStream.setGlobalStream(s);
-
-        %variables
-        nData = 600;
-        dimension = 1;
-        nTrainPoints = 100;
+            %variables
+            nData = 600;
+            nTrainPoints = 100;
     
-        X = randn(nData,dimension);
-        N = randn(nData,1);
-        w = randn(dimension,1);
-        Y = X*w + N; 
+            X = randn(nData,dimension);
+            N = randn(nData,1);
+            w = randn(dimension,1);
+            Y = X*w + N; 
   
-        trainX = X(1:nTrainPoints,:);
-        trainY = Y(1:nTrainPoints,:);
+            trainX = X(1:nTableTrainPoints(1,j),:);
+            trainY = Y(1:nTableTrainPoints(1,j),:);
 
-        testX = X(100:nData,:);
-        testY = Y(100:nData,:);
+            testX = X(nTableTrainPoints(1,j)+1:nData,:);
+            testY = Y(nTableTrainPoints(1,j)+1:nData,:);
 
-        %implementation of 5-fold cross-validation
-        k = 5;
-        [xSetsTrain, ySetsTrain, xSetsValidation, ySetsValidation] = kFoldCrossValidation(trainX,trainY,k);
-        
-        mseTrainAverage = 0;
-        mseTestAverage = 0;
-        for fold = 1 : k
-            smallerTrainX = xSetsTrain(:,fold);
-            smallerTrainY = ySetsTrain(:,fold);
-            validationX = xSetsValidation(:,fold);
-            validationY = ySetsValidation(:,fold);
-            percentage = (k-1) / k;
-            wTrained=(transpose(smallerTrainX)*smallerTrainX+gamma*percentage*nTrainPoints*eye(dimension))\(transpose(smallerTrainX)*smallerTrainY);
-            mseTrainAverage = mseTrainAverage + computeMSE(wTrained,trainX,trainY);
-            mseTestAverage = mseTestAverage + computeMSE(wTrained,testX,testY);
-        end
+            %implementation of cross-validation
+            k = 5;
+            [xSetsTrain, ySetsTrain, xSetsValidation, ySetsValidation] = kFoldCrossValidation(trainX,trainY,k);
 
-        mseTrainAverage = mseTrainAverage / k;
-        mseTestAverage = mseTestAverage / k;
-        
-        MSETrain(1,i) = mseTrainAverage;
-        MSETest(1,i) = mseTestAverage;
-        
-        vectorGamma(1,i)=gamma;
-        gamma=10*gamma;
-        averageTestError(1,i) = averageTestError(1,i) + MSETest(1,i);
-        averageTrainError(1,i) = averageTrainError(1,i) + MSETrain(1,i);
-    end;
-    if(toPlot)
+            mseTrainAverage = 0;
+            mseTestAverage = 0;
+            mseValidation = 0;
+            for fold = 1 : k
+                smallerTrainX = xSetsTrain(:,:,fold);
+                smallerTrainY = ySetsTrain(:,fold);
+                validationX = xSetsValidation(:,:,fold);
+                validationY = ySetsValidation(:,fold);
+                percentage = (k-1) / k;
+                wTrained(:,i)=(transpose(smallerTrainX)*smallerTrainX+gamma*percentage*nTrainPoints*eye(dimension))\(transpose(smallerTrainX)*smallerTrainY);
+   
+                mseTrainAverage = mseTrainAverage + computeMSE(wTrained(:,i),trainX,trainY);
+                mseTestAverage = mseTestAverage + computeMSE(wTrained(:,i),testX,testY);
+                mseValidation = mseValidation + computeMSE(wTrained(:,i),validationX,validationY);
+            end
 
-        figure
-        semilogx(vectorGamma,MSETrain, '-b')
-        hold on;
-        semilogx(vectorGamma, MSETest, '-g')
-    end;
-    
+            mseTrainAverage = mseTrainAverage / k;
+            mseTestAverage = mseTestAverage / k;
+            mseValidation = mseValidation / k;
+            
+            MSETrain(j,i) = mseTrainAverage;
+            MSETest(j,i) = mseTestAverage;
+            
+            MSEValidation(j,i) = mseValidation;
+            
+            vectorGamma(1,i)=gamma;
+            gamma=10*gamma;
+            averageValidationError(1,i) = averageValidationError(1,i) + MSEValidation(1,i);
+        end;
+        if(toPlot)
+            figure
+            semilogx(vectorGamma,MSEValidation(j,:), '-b')
+            title('Evolution of the error on the development set in function of gamma');
+        end;
+        [minimum,index]=min(MSEValidation(j,:));
+        indexaverage(1,j)=((i-1)*indexaverage(1,j)+index)./i;
+        resultsMatrix(1,j)=minimum;
+        resultsMatrix(2,j) = computeMSE(wTrained(:,index),testX,testY);
+     end;
 end;
-averageTestError=averageTestError./nLoops;
-averageTrainError=averageTrainError./nLoops;
-figure(2)
-semilogx(vectorGamma,averageTestError, '-b')
-hold on;
-semilogx(vectorGamma, averageTrainError, '-g')
+indexaverage(1,1)=10^(-6)*10^indexaverage(1,1);
+indexaverage(1,2)=10^(-6)*10^indexaverage(1,2);
+indexaverage
+resultsMatrix
+MSEValidation;
 
 
--- kFoldCrossValidation.m
+++ kFoldCrossValidation.m
@@ -1,17 +1,18 @@
 function [xSetsTrain, ySetsTrain, xSetsValidation, ySetsValidation] = kFoldCrossValidation(X,Y, k)
+dimension = size(X,2);
 nTrainPoints = size(X,1);
-xSetsTrain = zeros((k-1)/k * nTrainPoints, k);
+xSetsTrain = zeros((k-1)/k * nTrainPoints, dimension, k);
 ySetsTrain = zeros((k-1)/k * nTrainPoints, k);
-xSetsValidation = zeros(1/k * nTrainPoints, k);
+xSetsValidation = zeros(1/k * nTrainPoints, dimension, k);
 ySetsValidation = zeros(1/k * nTrainPoints, k);
 
 iter = 1;
 for i = 0 : 1/k : (k-1)/k
     xCopy = X;
     yCopy = Y;
-    xSetsValidation(:,iter) = X(i * nTrainPoints + 1: (i + 1/k) * nTrainPoints,:);
+    xSetsValidation(:,:,iter) = X(i * nTrainPoints + 1: (i + 1/k) * nTrainPoints,:);
     xCopy(i * nTrainPoints + 1: (i + 1/k) * nTrainPoints,:) = [];
-    xSetsTrain(:,iter) = xCopy;
+    xSetsTrain(:,:,iter) = xCopy;
     
     ySetsValidation(:,iter) = Y(i * nTrainPoints + 1: (i + 1/k) * nTrainPoints,:);
     yCopy(i * nTrainPoints + 1: (i + 1/k) * nTrainPoints,:) = [];
